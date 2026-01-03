7adc44f5dc857ae4fb664a3e553069e01ba19305
Prashant33-py <pvasala786@gmail.com>
2026-01-03 20:48:26 +0530

    Creates vector store db and completes asking questions part

--- src/main/java/com/spring/ai/ask/hr/service/ContextService.java
+++ src/main/java/com/spring/ai/ask/hr/service/ContextService.java
@@ -6,13 +6,16 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.mistralai.MistralAiChatModel;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -57,11 +60,23 @@ public class ContextService {
             pdfTextContents.put("page_" + i, cleanedText);
         }
         document.close();
-        createDocuments();
+        addDocumentsToVectorStore(createDocuments());
         System.out.println(pdfTextContents);
     }
 
-    private void createDocuments() {
+    private void addDocumentsToVectorStore(List<Document> documents) {
+        TokenTextSplitter splitter = TokenTextSplitter.builder()
+                .withChunkSize(60)
+                .withMinChunkSizeChars(50)
+                .withMinChunkLengthToEmbed(60)
+                .withMaxNumChunks(40)
+                .withKeepSeparator(false)
+                .build();
+
+        vectorStore.add(splitter.split(documents));
+    }
+
+    private List<Document> createDocuments() {
         List<Document> documents = new ArrayList<>();
         for (String pageKey : pdfTextContents.keySet()) {
             Document doc = Document.builder()
@@ -70,8 +85,9 @@ public class ContextService {
                     .metadata("source", filePath)
                     .metadata("page", pageKey)
                     .build();
+            documents.add(doc);
         }
-        vectorStore.add(documents);
+        return documents;
     }
 
     public void handlePdfUpload() throws MalformedURLException {
@@ -80,5 +96,14 @@ public class ContextService {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public ResponseEntity<String> askQuestion(String question){
+        String answer = mistralChatClient
+                .prompt(question)
+                .advisors(QuestionAnswerAdvisor.builder(vectorStore).build())
+                .call()
+                .content();
+        return ResponseEntity.ok().body(answer);
     }
 }
